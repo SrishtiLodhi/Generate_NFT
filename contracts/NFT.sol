2e88e8b0a15eb34e24547bccf70ed5a66435d292
Srishti Lodhi <srishtilodhi38@gmail.com>
2024-08-02 21:38:41 +0530

    main

---
+++ contracts/NFT.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyNFT is ERC721URIStorage {
+    uint256 public tokenCounter;
+
+    constructor() ERC721("MyNFT", "MNFT") {
+        tokenCounter = 0;
+    }
+
+    function createNFT(address to, string memory tokenURI) public returns (uint256) {
+        uint256 newItemId = tokenCounter;
+        _mint(to, newItemId);  
+        _setTokenURI(newItemId, tokenURI); 
+        tokenCounter++;
+        return newItemId;
+    }
+}
